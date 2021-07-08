d15787f16a4aff981d779c9e840b2e3dd2b6e5f0
Chris <chrischillogallid64@outlook.com>
2021-07-07 20:40:07 -0500

    feat: email change

--- javascript/script.js
+++ javascript/script.js
@@ -27,7 +27,7 @@
     $form.addEventListener("submit", (e) => {
         e.preventDefault();
         $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/chrischillogallid64@outlook.com", {
+        fetch("https://formsubmit.co/ajax/cxchillogallid64@gmail.com", {
             method: "POST",
             body: new FormData(e.target),
         })
